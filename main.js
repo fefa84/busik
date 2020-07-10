87a6674b18a93f8daeb71d530ae9ea4d808b9760
Ewa Mitra <ewa.mitra@gmail.com>
2020-07-10 16:02:21 +0200

    formal changes

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const test=document.querySelector('.test');
 test.innerHTML='Nie wiem co mam napisać, bo to tylko test'
+
+const firstName="Ewa"
+const lastName="Mitra"
+
+function greet (firstName, lastName) {
+    console.log (`Witam Cię serdecznie, ${firstName} ${lastName}!Cieszę się, że jesteś`)
+}
+
+greet(firstName, lastName);
+
